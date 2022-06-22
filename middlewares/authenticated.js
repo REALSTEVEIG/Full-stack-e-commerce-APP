452f0ca7256272fd19a9a0d82774a2add1db1ef3
REALSTEVEIG <101066723+REALSTEVEIG@users.noreply.github.com>
2022-06-22 17:13:00 +0100

    initial commit

--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -4,13 +4,13 @@ const {StatusCodes} = require('http-status-codes')
 const authMiddleware = async (req, res, next) => {
     const token = req.cookies.token
 
-    if (!token || token === "") {
+    if (!token) {
         return res.status(StatusCodes.UNAUTHORIZED).redirect('login')
     }
 
     try {
         const payload = jwt.verify(token, process.env.JWT_SECRET);
-        req.username = {userId : payload.userId, username : payload.userername}
+        req.username = {userId : payload.userId, username : payload.username}
         next()        
     } catch (error) {
        return res.status(StatusCodes.FORBIDDEN).redirect('login')
