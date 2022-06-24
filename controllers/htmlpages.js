3326269263a0d55e6ee5b2047b1fc94ae2365dd7
REALSTEVEIG <101066723+REALSTEVEIG@users.noreply.github.com>
2022-06-24 20:38:14 +0100

    initial commit

--- controllers/htmlpages.js
+++ controllers/htmlpages.js
@@ -1,5 +1,16 @@
 const jwt = require('jsonwebtoken')
 
+const nodemailer = require('nodemailer')
+//Begining of Oauth configuration
+const { google } = require('googleapis')
+const config = require('../config')
+const { StatusCodes } = require('http-status-codes')
+const OAuth2 = google.auth.OAuth2
+
+const OAuth2_client = new OAuth2(config.clientId, config.clientSecret)
+OAuth2_client.setCredentials({refresh_token : config.refreshToken})
+//End of Oauth2 configuration
+
 exports.index = async (req, res) => {
     const token = req.cookies.token
     try {
@@ -17,6 +28,59 @@ exports.about = (req, res) => {
 
 exports.contact = (req, res) => {
     res.render('contact')
+}
+
+exports.contactSend = (req, res) => {
+    const output = `
+        <h1>You have a new contact request</h1>
+        <h3>Contact Details</h3>
+        <ul>
+            <li>Name : ${req.body.name}</li>
+            <li>Email : ${req.body.email}</li>
+        </ul>
+        <h3>Message</h3>
+        <p>${req.body.message}</p>
+    `
+    const accessToken = OAuth2_client.getAccessToken
+    let transporter = nodemailer.createTransport({
+        service : 'gmail',
+        auth : {
+            type : 'OAuth2',
+            user : config.user,
+            clientId : config.clientId,
+            clientSecret : config.clientSecret,
+            refreshToken : config.refreshToken,
+            accessToken : accessToken
+        }
+    })
+
+    let mailOptions = {
+        from : `AGU Nigeria<${config.user}>`, // sender address
+        to: `<${req.body.email}>`, // list of receivers   
+        subject : 'Greetings!', // Subject line
+        text : `Hello ${req.body.name}. Thank you for contacting Agu Nigeria. We have received your message and will get back to you as soon as possible!`     
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        return res.render('contact', {msg:'Your message has been sent successfully. Please check your email!'});
+    });
+
+    let mailOptions2 = {        // This will send the mail to your email address
+        from : `FROM STEPHEN <${config.user}>`, // sender address
+        to: `<${config.user}>`, // list of receivers
+        subject: `Message from ${req.body.name}!`, // Subject line
+        html: output // html body
+    };
+
+    transporter.sendMail(mailOptions2, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+       return console.log('Sent')
+    });
 }
 
 exports.product = (req, res) => {
--- routes/htmlpages.js
+++ routes/htmlpages.js
@@ -2,12 +2,13 @@ const express = require('express')
 
 const router = express.Router()
 
-const {index, about, blog_list, contact, product, testimonial} = require('../controllers/htmlpages')
+const {index, about, blog_list, contact, contactSend, product, testimonial} = require('../controllers/htmlpages')
 
 router.route('/index').get(index)
 router.route('/about').get(about)
 router.route('/blog_list').get(blog_list)
 router.route('/contact').get(contact)
+router.route('/contact_send').post(contactSend)
 router.route('/product').get(product)
 router.route('/testimonial').get(testimonial)
 
